2afb1578c7426354ea63c0867925fbdf0d93b22d
Deepam Wagh <deepam.wagh@gmail.com>
2025-12-02 21:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function wait(delay) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
+async function showMessage() {
+  const text = document.getElementById("text").value;
+  const delay = Number(document.getElementById("delay").value);
+
+  await wait(delay);
+
+  document.getElementById("output").innerText = text;
+}
+document.getElementById("btn").addEventListener("click", showMessage);
